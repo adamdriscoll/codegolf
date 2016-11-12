c788153c3af2f9fcba8dda3ff210fbb3669ccdc5
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-11-12 08:53:07 +0100

    Updating Monaco. Fixing some issues with links. Code clean up.

--- src/CodeGolf/gulpfile.js
+++ src/CodeGolf/gulpfile.js
@@ -5,5 +5,7 @@ var _ = require('lodash'),
     gulp = require('gulp');
 
 gulp.task('copy-assets', function () {
-    gulp.src(['./node_modules/**/*']).pipe(gulp.dest('./wwwroot/node_modules'));
+    gulp.src(['./node_modules/monaco-editor/min/vs/**/*']).pipe(gulp.dest('./wwwroot/lib/monaco-editor'));
+    gulp.src(['./node_modules/react/dist/react-with-addons.min.js']).pipe(gulp.dest('./wwwroot/lib/react'));
+
 });
