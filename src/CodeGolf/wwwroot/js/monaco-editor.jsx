0b1171469c7139a3c711b25dcc0a9da05314ffb1
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-11-12 09:40:39 +0100

    React components for the monaco editor and the vote buttons.

---
+++ src/CodeGolf/wwwroot/js/monaco-editor.jsx
@@ -0,0 +1,49 @@
+﻿class MonacoEditor extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            editor: null
+        }
+    }
+    loadData() {
+        var self = this;
+        $.get(this.dataUrl, (data) => self.createEditor(data.content, data.language, self.props.readonly));
+    }
+
+    createEditor(contents, language, readonly) {
+        this.state.editor = monaco.editor.create(this.editorContainer,
+        {
+            value: contents,
+            language: language,
+            readOnly: readonly
+        });
+
+        this.state.editor.getModel().onDidChangeContent(this.handleOnContentChanged.bind(this));
+    }
+
+    handleOnContentChanged() {
+        var content = this.state.editor.getModel().getLinesContent().join('\n');
+
+        if (this.props.onContentChanged) {
+            this.props.onCOntentChanged(content);
+        }
+    }
+
+    componentDidMount() {
+        const self = this;
+        require.config({ paths: { 'vs': "/lib/monaco-editor/" } });
+        require(["vs/editor/editor.main"],
+            function () {
+                if (self.props.dataUrl) {
+                    loadData();
+                } else {
+                    createEditor(self.props.contents, self.props.langauge, self.props.readonly);
+                }
+            });
+    }
+
+    render() {
+        return (<div ref={(domNode) => this.editorContainer = domNode}></div>);
+    }
+}
---
+++ src/CodeGolf/wwwroot/js/vote-button.jsx
@@ -0,0 +1,105 @@
+﻿class VoteButton extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            voted: props.voted,
+            canVote: props.canVote
+        }
+    }
+
+    handleOnClick() {
+        if (this.state.canVote) {
+            if (this.props.onVoted) {
+                this.props.onVoted();
+            }
+        }
+
+        $.post(this.props.dataUrl,
+        {
+            item: this.props.itemId,
+            value: this.props.upvote ? 1 : -1
+        }, this.handleSuccessfulVote.bind(this));
+    }
+
+    handleSuccessfulVote() {
+        if (this.props.onVoted) {
+            this.props.onVoted();
+        }
+
+        this.state.voted = true;
+        this.setState(this.state);
+    }
+
+    render() {
+        var iconClass;
+        if (this.props.upvote) {
+            iconClass = "fa fa-arrow-up fa-lg";
+        } else {
+            iconClass = "fa fa-arrow-down fa-lg";
+        }
+
+        var style;
+        if (this.state.voted) {
+            style = "font-weigth: bold";
+        }
+
+        return (<span>
+                    <a onclick={this.handleOnClick.bind(this)}>
+                        <i className={iconClass} style={style}></i>
+                    </a>
+                </span>);
+    }
+}
+
+class VoteButtons extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            votes: 0,
+            upvoted: true,
+            downvoted: true
+        }
+
+        if (props.votes) {
+            this.state.votes = props.votes;
+        }
+    }
+
+    handleUpVote() {
+        handleVote(1);
+    }
+
+    handleDownVote() {
+        handleVote(-1);
+    }
+
+    handleVote(voteValue) {
+        var self = this;
+        $.post(this.props.dataUrl,
+            () => {
+                self.state.votes += voteValue;
+                self.state.upvoted = true;
+                self.state.downvoted = false;
+                self.setState(self.state);
+            });
+    }
+
+    render() {
+        const canUpvote = !this.state.upvoted;
+        const canDownvote = !this.state.downvoted;
+
+        return (<div className="row">
+                    <div className="col-md-3">
+                        <VoteButton upvote={true} onVoted={this.handleUpVote.bind(this)} canVote={canUpvote}/>
+                    </div>
+                    <div className="col-md-3">
+                        {this.state.votes}
+                    </div>
+                    <div className="col-md-3">
+                        <VoteButton upvote={false} onVoted={this.handleDownVote.bind(this)} canVote={canDownvote}/>
+                    </div>
+                </div>);
+    }
+}
