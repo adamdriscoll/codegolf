e07e556264ba9d3614b1d6275022edc3c660112d
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-11-29 11:01:02 +0100

    Adding courses repo.

---
+++ src/CodeGolf/Interfaces/Repository/ICourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeGolf.Models;
+
+namespace CodeGolf.Interfaces.Repository
+{
+    public interface ICourseRepository
+    {
+        Task Initialize();
+        Task<Course> Get(Guid id);
+        Task<Course> Get(string name);
+        Task<IEnumerable<Course>> Get();
+        Task Create(Course course);
+        Task Delete(Course course);
+    }
+}
---
+++ src/CodeGolf/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CodeGolf.Models
+{
+    public class Course : CodeGolfDocument
+    {
+        public List<Guid> ProblemIds { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public override DocumentType Type { get; }
+    }
+}
